97cd1874e982c1234af5ab65e05484e7e0fd525e
ilDug <dognini.marco@gmail.com>
2026-02-23 22:07:12 +0100

    feat: add navigation and section switching for demo app

--- projects/demo/src/app/app.ts
+++ projects/demo/src/app/app.ts
@@ -1,14 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { Errors } from './errors/errors';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'demo-root',
-    imports: [Errors],
+    imports: [Errors, CommonModule],
     templateUrl: './app.html',
 })
 export class App {
 
+    sections = [
+        "errors",
+        "drag-and-drop"
+    ]
+
+    section = signal(this.sections[0]);
 
 
 }
--- projects/demo/src/app/errors/errors.ts
+++ projects/demo/src/app/errors/errors.ts
@@ -1,20 +1,26 @@
 import { CommonModule, JsonPipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { schema, required, email, minLength, form, FormField, submit } from "@angular/forms/signals";
+import { schema, required, email, minLength, form, FormField, submit, validate } from "@angular/forms/signals";
 import { DagError } from 'dag-ngx';
 
 interface User {
-    name: string;
     email: string;
     password: string;
+    confirmPassword: string;
 }
 
 const userSchema = schema<User>(root => {
-    required(root.name, { message: 'Name is required' });
     email(root.email, { message: 'Email is invalid' });
     required(root.email, { message: 'Email is required' });
     minLength(root.password, 8, { message: 'Password must be at least 8 characters long' });
     required(root.password, { message: 'Password is required' })
+    required(root.confirmPassword, { message: 'Confirm Password is required' });
+    validate(root.confirmPassword, ({ value, valueOf }) => {
+        if (value() !== valueOf(root.password)) {
+            return { message: 'Passwords do not match', kind: 'passwordMismatch' };
+        }
+        return null;
+    });
 })
 
 
@@ -27,9 +33,9 @@ const userSchema = schema<User>(root => {
 export class Errors {
 
     user = signal<User>({
-        name: '',
         email: '',
-        password: ''
+        password: '',
+        confirmPassword: ''
     });
 
     form = form(this.user, userSchema);
