4a17ab4efa03fb4c46c624fc4acadba0a36d0326
ilDug <dognini.marco@gmail.com>
2026-02-25 20:12:02 +0100

    Setting up dragdrop

---
+++ projects/demo/src/app/dragdrop/dragdrop.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-dragdrop',
+  imports: [],
+  templateUrl: './dragdrop.html',
+  styleUrl: './dragdrop.scss',
+})
+export class Dragdrop {
+
+}
